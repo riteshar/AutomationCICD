26ecb68e3b95ba29f96c15b227985d076b91a636
riteshar <riteshsharmagn@gmail.com>
2024-06-15 17:42:25 +0530

    trigger the cicd

--- src/test/java/riteshsharma/tests/StandAloneTest.java
+++ src/test/java/riteshsharma/tests/StandAloneTest.java
@@ -20,6 +20,8 @@ public class StandAloneTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
                 //adding comments to push code in gitHub repository so jenkins CICD job can be triggered.
+		
+		
 
 		String productName = "ZARA COAT 3";
 		WebDriverManager.chromedriver().setup();
